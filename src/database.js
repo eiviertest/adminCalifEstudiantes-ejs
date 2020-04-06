5a46fc291ae2fc11049f23c63a6089e6842a7297
AlejandroVargas <32745553+eiviertest@users.noreply.github.com>
2020-04-06 11:24:22 -0500

    Conexion con MongoAtlas

--- src/database.js
+++ src/database.js
@@ -1,12 +1,12 @@
 const mongoose = require("mongoose");
 //Configuration of params to connection
-//const { MongoAtlasConnection } = process.env;
-//const MONGODB_URI_ATLAS = `mongodb+srv://${MongoAtlasConnection}`;
-const MONGODB_URI_LOCAL = 'mongodb://localhost/students';
+const { MongoAtlasConnection } = process.env;
+const MONGODB_URI_ATLAS = `mongodb+srv://${MongoAtlasConnection}`;
+//const MONGODB_URI_LOCAL = 'mongodb://localhost/students';
 
 //Connection to MongoAtlas
 mongoose
-  .connect(MONGODB_URI_LOCAL, {
+  .connect(MONGODB_URI_ATLAS, {
     useUnifiedTopology: true,
     useNewUrlParser: true,
   })
